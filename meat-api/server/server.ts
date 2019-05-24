8e73cf6d9694150e7ea0dcce6dc27a90e5d2aa09
Lucas Correia <lucascorreia-.-@live.com>
2019-05-23 21:38:16 -0300

    secao 5

--- meat-api/server/server.ts
+++ meat-api/server/server.ts
@@ -1,10 +1,20 @@
 import * as restify from 'restify'
+import * as mongoose from 'mongoose'
+
 import { environment } from '../common/environment'
 import { Router } from '../common/router'
+import { mergePatchBodyParser } from './merge-patch.parser'
 
 export class Server {
 
     application: restify.Server
+
+    initializeDb(): mongoose.MongooseThenable {
+        (<any>mongoose).Promise = global.Promise
+        return mongoose.connect(environment.db.url, {
+            useMongoClient: true
+        })
+    }
 
     initRoutes(routers: Router[]): Promise<any>{
         return new Promise((resolve, reject) => {
@@ -16,6 +26,8 @@ export class Server {
                 })
 
                 this.application.use(restify.plugins.queryParser())
+                this.application.use(restify.plugins.bodyParser())
+                this.application.use(mergePatchBodyParser)
 
                 //routes
                 for (let router of routers) {
@@ -33,6 +45,9 @@ export class Server {
     }
 
     bootstrap(routers: Router[] = []): Promise<Server>{
-        return this.initRoutes(routers).then(() => this)
+        return this.initializeDb().then(() =>
+                this.initRoutes(routers).then(() => this)
+            )
+            
     }
 }
