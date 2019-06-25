48bb96a6e088bec02672451aece0022135a80536
Lucas Correia <lucascorreia-.-@live.com>
2019-06-25 19:46:22 -0300

    fim do curso

--- meat-api/dist/server/server.js
+++ meat-api/dist/server/server.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const restify = require("restify");
 const mongoose = require("mongoose");
+const corsMiddleware = require("restify-cors-middleware");
 const logger_1 = require("../common/logger");
 const environment_1 = require("../common/environment");
 const merge_patch_parser_1 = require("./merge-patch.parser");
@@ -28,9 +29,18 @@ class Server {
                     options.key = fs.readFileSync(environment_1.environment.security.key);
                 }
                 this.application = restify.createServer(options);
+                const corsOptions = {
+                    preflightMaxAge: 10,
+                    origins: ['*'],
+                    allowHeaders: ['authorization'],
+                    exposeHeaders: ['x-custom-header']
+                };
+                const cors = corsMiddleware(corsOptions);
+                this.application.pre(cors.preflight);
                 this.application.pre(restify.plugins.requestLogger({
                     log: logger_1.logger
                 }));
+                this.application.use(cors.actual);
                 this.application.use(restify.plugins.queryParser());
                 this.application.use(restify.plugins.bodyParser());
                 this.application.use(merge_patch_parser_1.mergePatchBodyParser);
